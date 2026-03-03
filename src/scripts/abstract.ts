9ad64f9bba069173a4fb496adef9af7c0bd03a7d
Rin <162780442+BanaCubed@users.noreply.github.com>
2026-03-03 18:25:41 +1300

    internal: getReferenceFromCoordinatePair function

--- src/scripts/abstract.ts
+++ src/scripts/abstract.ts
@@ -2,6 +2,8 @@
  * Type used to refer to a 2D "array" in which all cells contents are a given type.
  *
  * This is not a 2D array and should not be used as such, since it allows non-integer and negative numbers.
+ *
+ * @see {@link getReferenceFromCoordinatePair}
  */
 export type CoordinateReference<K> = Record<number, Record<number, K>>;
 
@@ -13,4 +15,11 @@ export interface CoordinatePair {
 	x: number;
 	/** The value of the Y dimension. */
 	y: number;
+}
+
+export function getReferenceFromCoordinatePair<K>(
+	coordRef: CoordinateReference<K>,
+	coordPair: CoordinatePair,
+): K | undefined {
+	return coordRef[coordPair.x]?.[coordPair.y] ?? undefined;
 }
