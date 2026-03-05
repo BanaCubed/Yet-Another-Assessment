7fce9932fd5bfccef10449fb02a04ad7fb53b6a3
Rin <162780442+BanaCubed@users.noreply.github.com>
2026-03-06 09:13:10 +1300

    internal: removeCellData function

--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -27,6 +27,22 @@ export function addCellData(x: number, y: number, data: BoardCellEntityReference
 	(gameState.boardState.cellData[x]?.[y] as BoardCellEntityReference[])?.push(data);
 }
 
+export function removeCellData(x: number, y: number, data: BoardCellEntityReference) {
+	const foundIndexes: number[] = [];
+	(gameState.boardState.cellData[x]?.[y] as BoardCellEntityReference[])?.filter(
+		(predicate, index) => {
+			// If the function is overzealous turning this into a strict equality might help
+			if (predicate == data) {
+				foundIndexes.push(index);
+			}
+		},
+	);
+
+	foundIndexes.forEach(element => {
+		(gameState.boardState.cellData[x]?.[y] as BoardCellEntityReference[])?.splice(element, 1);
+	});
+}
+
 declare global {
 	interface Window {
 		game: GameState;
