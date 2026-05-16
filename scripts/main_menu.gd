c772a194ff0bf5c6dc852ce50e7cd7c8c3eb579d
Rin <162780442+BanaCubed@users.noreply.github.com>
2026-05-16 13:37:18 +1200

    feat: Main menu

---
+++ scripts/main_menu.gd
@@ -0,0 +1,13 @@
+class_name MainMenu extends Control
+## Control node specifically for the main menu.
+## Has little special funtionality besides connecting signals from the main menu buttons.
+
+
+func _on_exit_button_pressed() -> void:
+	# Godot recommends using get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST),
+	# but that doesn't seem to work.
+	get_tree().quit()
+
+
+func _on_play_button_pressed() -> void:
+	pass # Replace with function body.
